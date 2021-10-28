26904a6f8e054a37de6770d86ca7ad4a7ace6528
Kwafo Nana Mensah (kwafoDEV) <nanamensah11@yahoo.co.uk>
2021-10-29 00:28:17 +0100

    repository class setup

---
+++ Repositories/InMemItemRepository.cs
@@ -0,0 +1,36 @@
+
+using System.Collections.Generic;
+using Catalog.Entities;
+using System;
+using System.Linq;
+
+namespace Catalog.Repositories
+{
+
+public class InMemItemsRepository
+{
+       private readonly List<Item> items = new()
+       {
+           new Item {Id =Guid.NewGuid(),Name="Potion",Price =9,CreatedDate =DateTimeOffset.UtcNow},
+           new Item {Id =Guid.NewGuid(),Name="Potion",Price =9,CreatedDate =DateTimeOffset.UtcNow},
+           new Item {Id =Guid.NewGuid(),Name="Potion",Price =9,CreatedDate =DateTimeOffset.UtcNow}
+       };
+
+       public IEnumerable<Item> GetItems()
+       {
+           return items;
+       }
+
+        public Item GetItem(Guid id)
+        {
+        return items.Where(item =>item.Id == id).SingleOrDefault();
+        }
+
+    
+}
+
+
+
+
+
+}
