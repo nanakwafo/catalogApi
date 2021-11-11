23b1a72c16ca13a0dd9c469a6927cfafe275a472
nanakwafo <nanamensah1140@gmail.com>
2021-11-11 01:57:13 +0000

    Added dto class for Item

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,8 +1,11 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Linq;
 using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using Catalog.Dtos;
 
 namespace Catalog.Controllers
 {
@@ -18,21 +21,22 @@ namespace Catalog.Controllers
 
         //GET /items
         [HttpGet] 
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<ItemDto> GetItems()
         {
-             var items = repository.GetItems();
+             var items = repository.GetItems().Select(item => item.AsDto());
              return items;
         }
 
         //GET /items/id
         [HttpGet("{id}")]
-        public ActionResult<Item> GetItem(Guid id){
+        public ActionResult<ItemDto> GetItem(Guid id){
           var item = repository.GetItem(id);
+       
 
           if(item is null){
               return NotFound();
           }
-          return item;
+          return item.AsDto();
         }
     }
 }
